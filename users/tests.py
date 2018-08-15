9e4c974f877f694dfd3bae0741ec3c8128eb3670
Kevin Tovar <kevintovar@esimez.mx>
2018-08-15 08:30:16 -0500

    Create a simple test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+from .models import User
+
 # Create your tests here.
+class UserModelTest(TestCase):
+
+    def test_string_representation(self):
+        user = User(email="kevin@gmail.com")
+        self.assertEqual(str(user), user.email)
