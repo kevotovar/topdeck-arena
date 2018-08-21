cb19c9fb1d77a320cd05b273e32fbd3b89f7ee37
Kevin Tovar <kevintovar@esimez.mx>
2018-08-20 23:05:18 -0500

    Add logout view

--- users/views.py
+++ users/views.py
@@ -1,22 +1,11 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import logout
 from django.shortcuts import render, redirect
-from django.conf import settings
 
-from .forms import SignUpForm, LoginForm
-
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=email, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
 
 def login(request):
     return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
