c891c21ba707947cb55b3a345b06712d235a4da7
Kevin Tovar <kevintovar@esimez.mx>
2018-08-21 23:43:37 -0500

    Register to the django admin

--- teams/models.py
+++ teams/models.py
@@ -6,7 +6,7 @@ from users.models import User
 
 
 class Team(TimeStampedModel):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, unique=True)
     owner = models.ForeignKey(
         User, on_delete=models.PROTECT, related_name='teams'
     )
@@ -17,8 +17,14 @@ class Team(TimeStampedModel):
         through_fields=('team', 'user')
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Role(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     team = models.ForeignKey(Team, on_delete=models.PROTECT)
     active = models.BooleanField(_('active'), default=False)
+
+    def __str__(self):
+        return "{}: {} - {}".format(self.pk, self.user.name, self.team.name)
