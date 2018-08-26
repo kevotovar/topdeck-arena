6a5574420f74362946242c4c46324da7d57da01a
Kevin Tovar <kevintovar@esimez.mx>
2018-08-26 13:05:06 -0500

    Load fonts async

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,12 @@
+import WebFont from 'webfontloader';
+
 require('materialize-css');
+
+WebFont.load({
+  google: {
+    families: ['Material Icons'],
+  },
+});
 
 $(() => {
   $('.dropdown-trigger').dropdown({ hover: false });
